8888610c7ef885d36bc8f94126081d7b8b4fda31
sukeshrs <sukesrs@gmail.com>
2019-02-06 21:44:51 -0800

    added redux dependencies

---
+++ src/state/price/selectors.js
@@ -0,0 +1,8 @@
+import { createSelector } from "reselect";
+
+const price = state => state.price;
+export const usdPrice = createSelector(
+  price,
+  price => price && price.price_usd
+);
+export const loading = state => state.loading;
